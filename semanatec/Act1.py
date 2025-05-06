0f0b49449ab066aac8ce69d0e69e55e34970dfa0
EloyRb0 <eloyrb@outlook.es>
2025-05-06 08:29:14 -0600

    Modificacion de comentarios a ingles

--- semanatec/Act1.py
+++ semanatec/Act1.py
@@ -1,32 +1,30 @@
 import cv2
 import pytesseract
 
-# Configurar tesseract
-#pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
-# Cargar imagen
+# Load image and save in a variable
 img = cv2.imread("placa_4.jpg")
 
-# Escala de grises
+# Change image to greyscale
 gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Mejorar contraste
-clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+# Up the contrast for the image
+clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
 gris = clahe.apply(gris)
 
-# Suavizar con Gaussian Blur
+# Soften the image with Gaussian Blur filter
 suavizada = cv2.GaussianBlur(gris, (5, 5), 0)
 
-# Bordes
+# Border detection 
 bordes = cv2.Canny(suavizada, 100, 200)
 cv2.imshow("Bordes (Canny)", bordes)
 
-# OCR
+# OCR using Tesseract 
 texto_detectado = pytesseract.image_to_string(suavizada, config='--psm 8')
 
-print("Texto detectado:", texto_detectado.strip())
+# Print detected text in console
+print(f"Texto detectado: {texto_detectado.strip()}")
 
-# Mostrar resultado visual
+# Show visual results
 cv2.imshow("Entrada", img)
 cv2.imshow("Mejorada", suavizada)
 cv2.waitKey(0)
